36fa5123da24174a7a55970f374a5e5b83a7bece
glixzzy <itrxpzz0@gmail.com>
2021-12-04 18:08:10 -0600

    add gold amount param

--- src/assets/messages.js
+++ src/assets/messages.js
@@ -12,12 +12,12 @@ module.exports = {
             return JSON.stringify({ "t": "d", "d": { "r": 6, "a": "p", "b": { "p": `/` + gamePin + `/c/` + botName, "d": { "b": blook, "g": 999999 } } } })
         },
 
-        steal: (gamePin, botName, randomBlook, victimName) => {
-            return JSON.stringify({ "t": "d", "d": { "r": 18, "a": "p", "b": { "p": "/" + gamePin + "/c/" + botName, "d": { "b": randomBlook, "g": 0, "tat": victimName + ":999999" } } } })
+        steal: (gamePin, botName, randomBlook, victimName, goldAmount) => {
+            return JSON.stringify({ "t": "d", "d": { "r": 18, "a": "p", "b": { "p": "/" + gamePin + "/c/" + botName, "d": { "b": randomBlook, "g": 0, "tat": victimName + ":" + goldAmount } } } })
         },
 
-        give: (gamePin, botName, randomBlook, victimName) => {
-            return JSON.stringify({ "t": "d", "d": { "r": 18, "a": "p", "b": { "p": "/" + gamePin + "/c/" + botName, "d": { "b": randomBlook, "g": 0, "tat": victimName + ":swap:999999" } } } })
+        give: (gamePin, botName, randomBlook, victimName, goldAmount) => {
+            return JSON.stringify({ "t": "d", "d": { "r": 18, "a": "p", "b": { "p": "/" + gamePin + "/c/" + botName, "d": { "b": randomBlook, "g": 0, "tat": victimName + ":swap:" + goldAmount } } } })
         },
     },
 
