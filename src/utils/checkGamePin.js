0b648993520be1b0bbf79e20290792a15578c98c
glixzzy <itrxpzz0@gmail.com>
2021-12-08 21:03:51 -0600

    v2 update released

--- src/utils/checkGamePin.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-
-async function isGameAlive(gamePin) {
-    const response = await axios('https://api.blooket.com/api/firebase/id?id=' + gamePin);
-
-    return response.data
-};
-
-module.exports = isGameAlive
