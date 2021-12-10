0cda740b09bf7623e06527fdd37f0aca32d1e7a8
gimmy74 <72813434+gimmy74@users.noreply.github.com>
2021-12-09 23:49:00 -0500

    Update floodGame.js

--- tests/floodGame.js
+++ tests/floodGame.js
@@ -2,7 +2,7 @@ const Blooket = require('../index');
 
 const client = new Blooket();
 
-client.floodGame('922159', 100)
+client.floodGame('922159', 'glixzzy', 100)
 
 client.on('flood', data => {
     console.log('Joined game with name: ' + data.player);
